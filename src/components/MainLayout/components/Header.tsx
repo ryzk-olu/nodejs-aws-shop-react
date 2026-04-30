be4404b18876cb2285fa1c1e0eed5d5f4a0d5173
Olga Ryzhkova <olga.ryzhkova.v@gmail.com>
2026-04-30 20:15:53 +0400

    Header modified

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            RS School Store
           </Link>
         </Typography>
 
