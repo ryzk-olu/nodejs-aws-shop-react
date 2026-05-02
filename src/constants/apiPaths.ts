c741e906cbc85d9e36f7b2b4d77444aec57fef43
Olga Ryzhkova <olga.ryzhkova.v@gmail.com>
2026-05-02 17:15:24 +0400

    Links updated

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://de4dpsthh5.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-//  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff:  "https://de4dpsthh5.execute-api.us-east-1.amazonaws.com/prod/",
+  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
