dbf7b60f9d8a4bbb29b8e87d98cb6f0e041b5f5a
Olga Ryzhkova <olga.ryzhkova.v@gmail.com>
2026-05-02 16:05:29 +0400

    API Urs changed

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,8 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+//  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff:  "https://de4dpsthh5.execute-api.us-east-1.amazonaws.com/prod/",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
