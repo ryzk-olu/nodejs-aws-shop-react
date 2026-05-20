f5df604de1c8845a1d89d455a3b3dcea3b6d1283
Olga Ryzhkova <olga.ryzhkova.v@gmail.com>
2026-05-20 22:02:14 +0400

    Links changed

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://de4dpsthh5.execute-api.us-east-1.amazonaws.com/prod",
+  product: "https://9sgh7nl7v7.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
