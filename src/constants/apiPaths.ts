1375de9022681f072370cf57024898b00fb8d33d
Olga Ryzhkova <olga.ryzhkova.v@gmail.com>
2026-06-16 02:07:00 +0400

    Api path updated

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://g29g6tymtg.execute-api.us-east-1.amazonaws.com/prod",
-  order: "https://k3pk4y4ng4.execute-api.us-east-1.amazonaws.com/prod/api",
+  order: "https://d3pcdfbmz4mn2v.cloudfront.net/api",
   import: "https://5j6pao60we.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://k3pk4y4ng4.execute-api.us-east-1.amazonaws.com/prod/api",
+  cart: "https://d3pcdfbmz4mn2v.cloudfront.net/api",
 };
 
 export default API_PATHS;
