329da1a32094f2735014effb15ffba84118931ac
Olga Ryzhkova <olga.ryzhkova.v@gmail.com>
2026-06-17 00:20:04 +0400

    Api links updated

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://g29g6tymtg.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://d3pcdfbmz4mn2v.cloudfront.net/api",
   import: "https://5j6pao60we.execute-api.us-east-1.amazonaws.com/prod",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://d3jhks8jz99u8z.cloudfront.net",
   cart: "https://d3pcdfbmz4mn2v.cloudfront.net/api",
 };
 
